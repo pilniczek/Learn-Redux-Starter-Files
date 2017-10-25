5932ae8f1169df948f923afc77b9597c51eb99a4
Tom Pilnaj <pilnaj.t@gmail.com>
2017-10-25 23:57:37 +0200

    arrfunc wont re-render but class will

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Main = React.createClass({
+    render() {
+        return (
+            <Link to='/'><h1>ReduxInstagram</h1></Link>
+        )
+    }
+})
+
+export default Main
+
+/* This will not re-render after save
+const Main = () => (
+    <Link to='/'><h1>ReduxInstagram</h1></Link>
+)*/
+/*
+Warning: Accessing createClass via the main React package is deprecated,
+and will be removed in React v16.0. Use a plain JavaScript class instead.
+If you're not yet ready to migrate, create-react-class v15.* is available
+on npm as a temporary, drop-in replacement.
+For more info see https://fb.me/react-create-class
+*/
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = React.createClass({
+    render() {
+        return (
+            null
+        )
+    }
+})
+
+export default Main
